ffd506c5ca8791fe2462b77f38242b164d5c344a
einsteinder <einsteinder@gmail.com>
2017-12-24 00:37:00 -0500

    add seen

--- 695. Max Area of Island.py
+++ 695. Max Area of Island.py
@@ -1,8 +1,5 @@
-import sys
-sys.setrecursionlimit(1500)
 
-
-xgrid1=[[0,0,1,0,0,0,0,1,0,0,0,0,0],
+xgrid=[[0,0,1,0,0,0,0,1,0,0,0,0,0],
  [0,0,0,0,0,0,0,1,1,1,0,0,0],
  [0,1,1,0,1,0,0,0,0,0,0,0,0],
  [0,1,0,0,1,1,0,0,1,0,1,0,0],
@@ -11,33 +8,30 @@ xgrid1=[[0,0,1,0,0,0,0,1,0,0,0,0,0],
  [0,0,0,0,0,0,0,1,1,1,0,0,0],
  [0,0,0,0,0,0,0,1,1,0,0,0,0]]
 
-xgrid=[[0,0,1,1,1,0,0]]
+
 class Solution(object):
     def maxAreaOfIsland(self, grid):
+        seen = set()
         maxCount = 0
         for i in range(len(grid)):
-            for j in range(len(grid[i])):
-                count = self.checkToZero(grid,i,j)
+            for j in range(len(grid[0])):
+                count = self.checkToZero(grid,i,j,seen)
                 if count > maxCount:
                     maxCount = count
-                print(count)
+
         return maxCount
 
-    def checkToZero(self,grid,x,y):
-        count = 0
-        print("x:",x,"y:",y)
-        if grid[x][y]:
-            count+=1
-            count += self.checkToZero(grid,x,y-1)
-            count += self.checkToZero(grid,x,y+1)
-            count += self.checkToZero(grid,x+1,y)
-            count += self.checkToZero(grid,x-1,y)
-            return count            
+    def checkToZero(self,grid,x,y,seen):
+        
+
+        if x>=len(grid) or x<0 or y>= len(grid[0]) or y<0:return 0    
+        if not (grid[x][y] and (0<= x <len(grid)) and (x,y) not in seen and (0<= y< len(grid[0]))):
+
+            return 0           
 
         else:
-            return count
-
-
+            seen.add((x,y))
+            return 1+self.checkToZero(grid,x+1,y,seen)+self.checkToZero(grid,x-1,y,seen)+self.checkToZero(grid,x,y+1,seen)+self.checkToZero(grid,x,y-1,seen)
 x = Solution()
 result=x.maxAreaOfIsland(xgrid)
 print(result)
