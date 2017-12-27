3ef94f20b2424ea296417a7b5c6148bd7a087da4
einsteinder <einsteinder@gmail.com>
2017-12-27 16:10:47 -0500

    git commit -m

--- 167. Two Sum II - Input array is sorted.py
+++ 167. Two Sum II - Input array is sorted.py
@@ -1,13 +1,44 @@
 class Solution:
-    def twoSum(self, numbers, target):
-    	for i in range(len(numbers)):
-    		difference = target - numbers[i]
-    		for j in range(len(numbers[i+1:])):
-    			if numbers[i+1:][j]==difference:
-    				return (i+1,i+1+j+1)
+	def twoSum(self, numbers, target):
+		differenceList = []
+		for i in range(len(numbers)):
+			difference = target - numbers[i]
+			differenceList.append(difference)
+		otherElement = set(differenceList)&set(numbers)
+		otherElement = list(otherElement)
+		otherRealElement=[]
+		print("other",otherElement)
+		for item in otherElement:
+			if item == target/2:
+				if numbers.count(item)==1:
+					continue
+				else:
+					otherRealElement.append(item)
+			print("oteer",otherRealElement)
+			otherRealElement.append(item)
+
+		result = []
+		count = 0
+		otherRealElement.sort()
+		print(otherRealElement)
+
+		for item in otherRealElement:
+
+			print("number",numbers)
+			result.append(numbers.index(item)+count)
+			print("item",item)
+			print("numbers.index(item)",numbers.index(item))
+			print("count",count)
+			print("result",result)
+			numbers.remove(item)
+			count+=1
+
+		return [x+1 for x in result]
+
+
 
 x = Solution()
-print(x.twoSum([2,7,3,4],9))
+print(x.twoSum([0,4,0,4],0))
 
 
         
