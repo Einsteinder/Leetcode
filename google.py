b70b4e851b0c64565b27b4d8503c6d4d383b7b81
einsteinder <einsteinder@gmail.com>
2018-02-23 01:18:39 -0500

    google 2

--- google.py
+++ google.py
@@ -1,31 +1,46 @@
 """
-Inputs:
-    (string) s = "abccbaabccba"
-Output:
-    (int) 2
-
-Inputs:
-    (string) s = "abcabcabcabc"
-Output:
-    (int) 4
+For example, given minion ID n = 1211, k = 4, b = 10, then x = 2111, y = 1112 and z = 2111 - 1112 = 0999. Then the next minion ID will be n = 0999 and the algorithm iterates again: x = 9990, y = 0999 and z = 9990 - 0999 = 8991, and so on.
+[210111, 122221, 102212]
 """
-s = "eeeeeeaaaaff"
-def brute(inputString):
-	resultDic = {}
-	for i in range(1,int(len(inputString))):
-		resultDic[i]=[]
-		for j in range(0,len(inputString),i):
+number = 210111
+count = 0 
+def answer(n, b):
+	resultList = []
+	def inanswer(n,b):
+		originalN =n
+		numberList = []
+		for i in str(n):
+			numberList.append(i)
+		x = sorted(numberList,reverse=True)
+		y = sorted(numberList)
+		zList =[]
+		lendOne = 0
+		for index in range(len(x)-1,-1,-1):
+			x[index]=int(x[index])-lendOne
+			if int(x[index]) >= int(y[index]):
+				resultNumber = int(x[index])-int(y[index])
+				lendOne = 0
+			else:
+				resultNumber = b-(int(y[index])-int(x[index]))
+				lendOne = 1
+			zList.insert(0,resultNumber)
+			zList =[str(i) for i in zList]
+		n="".join(zList)
+		print(n)
+		b=b
+		if n in resultList:
+			print(resultList.index(n))
+			print(len(resultList[resultList.index(n):]))
+		else:
+			resultList.append(n)
+			inanswer(n,b)
 
-			resultDic[i].append(inputString[j:j+i])
-	newDic = {}
-	for item in resultDic.items():
-		newDic[item[0]]=set(item[1])
-	result = min(newDic.items(),key=lambda x:len(x[1]))
-	print(result)
-	if len(result[1])==1:
-		return int(len(inputString)/result[0])
-	else:
-		return 1
 
-print(brute(s))
+		
+	inanswer(n,b)
+	print(resultList)
+
+
+
+print(answer(210022,3))
 
