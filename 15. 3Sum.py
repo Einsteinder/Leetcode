0240c4e89aaf8e0e389d348d4332f4e7bd73d163
einsteinder <einsteinder@gmail.com>
2018-07-28 10:24:55 -0400

    15 TLE

--- 15. 3Sum.py
+++ 15. 3Sum.py
@@ -1,10 +1,14 @@
-
-S = [-1, 0, 1, 2, -1, -4],
-
 class Solution:
     def threeSum(self, nums):
+        result = []
+        for i in range(len(nums)-2):
+            for j in range(i+1, len(nums)-1):
+                for k in range(j+1, len(nums)):
+                    if nums[i] + nums[j] + nums[k] == 0:
+                        result.append(tuple(sorted([nums[i], nums[j], nums[k]])))
+        return list(set(result))
 
-        """
-        :type nums: List[int]
-        :rtype: List[List[int]]
-        """
+so = Solution()
+print(so.threeSum([-1, 0, 1, 2, -1, -4]))
+
+
