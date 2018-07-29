31fa3bce2d5b21de2e7b38772fcdd8b8261264b3
einsteinder <einsteinder@gmail.com>
2018-07-28 23:25:36 -0400

    15 AC

--- 15. 3Sum.py
+++ 15. 3Sum.py
@@ -1,12 +1,30 @@
-class Solution:
+class Solution(object):
     def threeSum(self, nums):
+        sortedNums = sorted(nums)
         result = []
-        for i in range(len(nums)-2):
-            for j in range(i+1, len(nums)-1):
-                for k in range(j+1, len(nums)):
-                    if nums[i] + nums[j] + nums[k] == 0:
-                        result.append(tuple(sorted([nums[i], nums[j], nums[k]])))
-        return list(set(result))
+        for i in range(len(sortedNums) - 2):
+            j = i + 1
+            k = len(sortedNums) - 1
+            sumOfTwo = 0 - sortedNums[i]
+            if i == 0 or sortedNums[i] != sortedNums[i - 1]:
+                while j < k:
+                    if sortedNums[j] + sortedNums[k] == sumOfTwo:
+                        result.append([sortedNums[i], sortedNums[j], sortedNums[k]])
+                        while (j < k and sortedNums[j] == sortedNums[j + 1]):
+                            j += 1
+                        while (j < k and sortedNums[k] == sortedNums[k - 1]):
+                            k -= 1
+                        j += 1
+                    elif sortedNums[j] + sortedNums[k] < sumOfTwo:
+                        j += 1
+                    else:
+                        k -= 1
+        return list(result)
+
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
 
 so = Solution()
 print(so.threeSum([-1, 0, 1, 2, -1, -4]))
