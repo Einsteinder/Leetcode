2fbb87848928e2223317b2f285702e57b400b7df
einsteinder <einsteinder@gmail.com>
2018-08-26 08:54:50 -0400

    160 AC

--- TT.py
+++ TT.py
@@ -1,4 +1,34 @@
-from collections import deque
-queue, visited = deque([("begin", 1)]), set()
-queue.append([33,33])
-print(queue)
+
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution(object):
+    def getIntersectionNode(self, headA, headB):
+        a={}
+        b={}
+        pb = headB
+        while headA:
+            a[id(headA)] = headA
+            headA = headA.next
+        print(a)
+        for key,value in a.items():
+            while pb:
+                print(id(pb))
+                if id(pb) == key:
+                    print(pb.val)
+                    return pb
+                pb = pb.next
+            pb = headB
+        return None
+a = ListNode(1)
+b = ListNode(2)
+c = ListNode(3)
+c.next = a
+a.next = b
+e = c
+f = c
+
+so = Solution()
+so.getIntersectionNode(e,e)
